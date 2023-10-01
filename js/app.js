4d500231456faa265e04283a2875af481313e226
William França <will.franca1998@gmail.com>
2023-10-01 15:07:35 -0300

    modified:   css/global.css 	modified:   css/responsive.css 	new file:   img/bgsection.webp 	modified:   index.html 	modified:   js/app.js 	modified:   js/vanilla-tilt.min.js 	modified:   style.css

--- js/app.js
+++ js/app.js
@@ -20,6 +20,9 @@ const myObserver = new IntersectionObserver((entries) => {
 
 myObserver.observe(lastOne); */
 
+
+/* efeito mouse */
+
 const blurElement = document.getElementById('spotlight');
 
 document.addEventListener('mousemove', (e) => {
@@ -29,3 +32,42 @@ document.addEventListener('mousemove', (e) => {
     blurElement.style.left = mouseX - blurElement.clientWidth / 2 + 'px';
     blurElement.style.top = mouseY - blurElement.clientHeight / 2 + 'px';
 });
+
+// Função para calcular a distância entre dois elementos
+function calcularDistancia(element1, element2) {
+    const rect1 = element1.getBoundingClientRect(); // Obtém as coordenadas do element1
+    const rect2 = element2.getBoundingClientRect(); // Obtém as coordenadas do element2
+
+    const bottom1 = rect1.bottom; // Coordenada bottom de element1
+    const top2 = rect2.top; // Coordenada top de element2
+
+    // Calcula a distância subtraindo o bottom de element1 do top de element2
+    const distancia = top2 - bottom1;
+
+    return distancia;
+}
+
+
+// Chame a função para calcular a distância entre os elementos
+const cardPerfil = document.getElementById("card-perfil");
+const iconTec = document.getElementById("icon-tec");
+
+const distanciaEntreElementos = calcularDistancia(cardPerfil, iconTec);
+
+// Atualize o tamanho da linha vertical de acordo com a distância
+const linhaVertical = document.querySelector(".line-y");
+linhaVertical.style.height = distanciaEntreElementos + "px";
+
+const cardPerfilRect = cardPerfil.getBoundingClientRect();
+const positionY = cardPerfilRect.bottom + window.scrollY;
+
+linhaVertical.style.top = positionY - 20 + "px";
+
+// alterar tamanho do conteudo das linhas
+
+const lineBox = document.querySelector(".line-box");
+const cardPerfilWidth = cardPerfil.offsetWidth;
+
+lineBox.style.width = cardPerfilWidth + "px";
+
+
